65463b5d2df100aa1cf7b92617df2e403e973740
Ohohsiol <benjaminhwang04@gmail.com>
2024-01-28 15:35:05 +0800

    testing 12

--- S10257176_PRG2Assignment/obj/Debug/net8.0/S10257176_PRG2Assignment.AssemblyInfo.cs
+++ S10257176_PRG2Assignment/obj/Debug/net8.0/S10257176_PRG2Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S10257176_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad079dc349809ff7365868508f711dc251582f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2cc3fac4793000f024ad706260f39cc1772986d")]
 [assembly: System.Reflection.AssemblyProductAttribute("S10257176_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S10257176_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
